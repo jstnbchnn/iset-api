b4185c2e546744b44f3f328f871b04420315d884
Justin Buchanan <jstnbchnn@gmail.com>
2018-07-04 15:49:04 -0400

    Refactors shared functions

---
+++ src/lib/utils.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+const Builder = require('./urlBuilder');
+
+const API_BASE_URL = 'http://www.iset.net/TournamentSPA';
+
+exports.getAllDivisions = async () => {
+  const divisionData =
+    await this.makeGetRequest({ url: Builder.urlBuilder([API_BASE_URL, 'getTournamentDivisions'], { tid: 2267 })});
+
+  return divisionData.map(division => {
+    const { programDivisionName, pk: id } = division;
+
+    return {
+      id,
+      name: programDivisionName
+    };
+  });
+};
+
+exports.makeGetRequest = async (options) => {
+  let { data } = await axios(options);
+
+  return data;
+};
