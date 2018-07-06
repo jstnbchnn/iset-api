7db8525e4646a50ae4eb25791d7c0f7006a08cdc
Justin Buchanan <jstnbchnn@gmail.com>
2018-07-06 11:48:43 -0400

    Adds mocked response files

---
+++ test/fixtures/responses/divisionInfoData.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    'pk': 5905,
+    'name': 'Week 1 & 2',
+    'type': 1,
+    'date': 'Thursday, May 31, 2018'
+  },
+  {
+    'pk': 5927,
+    'name': 'Week 3 & 4',
+    'type': 1,
+    'date': 'Thursday, June 7, 2018'
+  },
+  {
+    'pk': 5928,
+    'name': 'Week 5 & 6',
+    'type': 1,
+    'date': 'Thursday, June 21, 2018'
+  }
+];
---
+++ test/fixtures/responses/teamsPageData.js
@@ -0,0 +1,212 @@
+module.exports = {
+  'divisions': [
+    {
+      'name': 'SEMI-Thursday 4\'s Semi-Competitive (early)',
+      'programDivisionName': null,
+      'maxTeams': null,
+      'elegibility': null,
+      'teams': [
+        {
+          'pk': 37474,
+          'divisionId': 3483,
+          'teamId': 38202,
+          'name': '"@therealdonalddumps"',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37475,
+          'divisionId': 3483,
+          'teamId': 38203,
+          'name': '2 Drink Minimum',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37464,
+          'divisionId': 3483,
+          'teamId': 38188,
+          'name': 'Bae Watch',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37469,
+          'divisionId': 3483,
+          'teamId': 38194,
+          'name': 'Beach Cobbler',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37477,
+          'divisionId': 3483,
+          'teamId': 38205,
+          'name': 'Beerff',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37473,
+          'divisionId': 3483,
+          'teamId': 38201,
+          'name': 'Block Obamas',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37564,
+          'divisionId': 3483,
+          'teamId': 38213,
+          'name': 'Block Party',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37560,
+          'divisionId': 3483,
+          'teamId': 38208,
+          'name': 'Blockbusters',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37478,
+          'divisionId': 3483,
+          'teamId': 38206,
+          'name': 'Brock Solutions',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37468,
+          'divisionId': 3483,
+          'teamId': 38193,
+          'name': 'Fire It Up!',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37430,
+          'divisionId': 3483,
+          'teamId': 38153,
+          'name': 'Hit Faced',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37565,
+          'divisionId': 3483,
+          'teamId': 38212,
+          'name': 'Just the Tip',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37472,
+          'divisionId': 3483,
+          'teamId': 38197,
+          'name': 'MIND BLOCKS',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37470,
+          'divisionId': 3483,
+          'teamId': 38195,
+          'name': 'No Class',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37476,
+          'divisionId': 3483,
+          'teamId': 38204,
+          'name': 'Panic Attack',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37467,
+          'divisionId': 3483,
+          'teamId': 38192,
+          'name': 'Powerhouse',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37465,
+          'divisionId': 3483,
+          'teamId': 38189,
+          'name': 'Sand Devils',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37462,
+          'divisionId': 3483,
+          'teamId': 38186,
+          'name': 'Sand Ninjas',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37431,
+          'divisionId': 3483,
+          'teamId': 38154,
+          'name': 'Serves You Right',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37471,
+          'divisionId': 3483,
+          'teamId': 38196,
+          'name': 'The Backsetters',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37466,
+          'divisionId': 3483,
+          'teamId': 38191,
+          'name': 'The Thursdays',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        },
+        {
+          'pk': 37463,
+          'divisionId': 3483,
+          'teamId': 38187,
+          'name': 'Unprotected Sets',
+          'hometown': null,
+          'province': 'ON',
+          'country': 'CAN'
+        }
+      ]
+    }
+  ],
+  'publishTeams': true,
+  'teamsFound': true
+};
